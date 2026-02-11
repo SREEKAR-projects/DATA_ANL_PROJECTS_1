c0931d69e2d657379a6511e1e1e7d262cda4936f
SREEKAR-projects <sreekarrv21@gmail.com>
2026-02-11 22:02:04 +0530

    Update

--- PYTHON_PROJECTS/Data Stats Calculator.py
+++ PYTHON_PROJECTS/Data Stats Calculator.py
@@ -4,3 +4,9 @@ numbers = input("Enter numbers separated by commas: ")
 
 data = numbers.split(",")
 data = [int(n) for n in data]
+
+total = sum(data)
+count = len(data)
+average = total / count
+minimum = min(data)
+maximum = max(data)
