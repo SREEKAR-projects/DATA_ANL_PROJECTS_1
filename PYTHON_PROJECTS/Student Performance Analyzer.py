c1b8797e00a4189f315842141fff5254c61e4539
SREEKAR-projects <sreekarrv21@gmail.com>
2026-02-12 22:13:00 +0530

    Update

--- PYTHON_PROJECTS/Student Performance Analyzer.py
+++ PYTHON_PROJECTS/Student Performance Analyzer.py
@@ -3,5 +3,11 @@ def student_stat():
     marks = marks_input.split(",")
     marks = [int(m) for m in marks]
     
+    total_students = len(marks)
+    total_marks = sum(marks)
+    average_marks = total_marks / total_students
+    highest_mark = max(marks)
+    lowest_mark = min(marks)
+
 
 
