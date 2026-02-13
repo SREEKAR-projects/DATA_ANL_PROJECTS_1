56a522d93a939b12a94c31ac9529d7772cd1072b
SREEKAR-projects <sreekarrv21@gmail.com>
2026-02-13 22:29:21 +0530

    Update

--- PYTHON_PROJECTS/Sales Data Analyzer (Mini Business Case).py
+++ PYTHON_PROJECTS/Sales Data Analyzer (Mini Business Case).py
@@ -1,3 +1,9 @@
 print("=== Sales Data Analyzer ===")
 
 sales_input = input("Enter daily sales separated by commas: ")
+if not sales_input.strip():
+    print("No sales data provided")
+    exit()
+
+sales = sales_input.split(",")
+sales = [float(s) for s in sales]
