8e1e310db6914f3c364c464f00dde545d5e359df
SREEKAR-projects <sreekarrv21@gmail.com>
2026-02-18 22:41:49 +0530

    Update

---
+++ PYTHON_PROJECTS/QuickBrew Order Manager.py
@@ -0,0 +1,11 @@
+# The central storage for our active orders
+order_queue = []
+
+# Available products and prices
+MENU = {
+    "Espresso": 3.00,
+    "Latte": 4.50,
+    "Cappuccino": 4.00
+}
+
+print("QuickBrew System Initialized.")
